d0b97688a71882797281e3570ea48cf2ec6d428c
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2019-04-19 22:08:47 -0300

    Adjusting final details
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>
    Co-authored-by: André Lucas de Sousa Pinto <andrelucax@gmail.com>

--- main.py
+++ main.py
@@ -37,6 +37,11 @@ class BloodMessage:
 		if(self.WorldNumber == other.WorldNumber):
 			return self.Owner < other.Owner
 		return self.WorldNumber < other.WorldNumber
+		
+	def __le__(self, other):
+		if(self.WorldNumber == other.WorldNumber):
+			return self.Owner <= other.Owner
+		return self.WorldNumber <= other.WorldNumber
 
 file = open("BloodMessages.json", "r")
 jsondata = file.read()
@@ -48,7 +53,9 @@ messages = []
 for message in data:
 	messages.append(BloodMessage(message['Owner'], message['WorldNumber'], message['XCoord'], message['YCoord'], message['ZCoord'], message['Text']))
 
+mergeIteration = 0
 def mergeSort(arr): 
+	global mergeIteration
 	if len(arr) > 1: 
 		mid = len(arr)//2 
 		L = arr[:mid] 
@@ -60,37 +67,41 @@ def mergeSort(arr):
 		i = j = k = 0
           
 		while i < len(L) and j < len(R): 
-			if L[i]['Owner'] < R[j]['Owner']: 
+			if L[i] < R[j]: 
 				arr[k] = L[i]
+				mergeIteration+=1
 				i+=1
 			else: 
 				arr[k] = R[j]
+				mergeIteration+=1
 				j+=1
 			k+=1
           
 		while i < len(L): 
 			arr[k] = L[i]
+			mergeIteration+=1
 			i+=1
 			k+=1
           
 		while j < len(R): 
 			arr[k] = R[j]
+			mergeIteration+=1
 			j+=1
 			k+=1
 			
 
-#quickIteration = 0
+quickIteration = 0
 def partition(arr,low,high): 
-	#global quickIteration
+	global quickIteration
 	i = (low-1)
 	pivot = arr[high]    
 	for j in range(low , high): 
-		if arr[j]['Owner'] <= pivot['Owner']: 
+		if arr[j] <= pivot: 
 			i = i+1 
 			arr[i],arr[j] = arr[j],arr[i] 
-			#quickIteration+=2
+			quickIteration+=2
 	arr[i+1],arr[high] = arr[high],arr[i+1]
-	#quickIteration+=2
+	quickIteration+=2
 	return (i+1) 
 
 
@@ -102,19 +113,19 @@ def quickSort(arr,low,high):
 		quickSort(arr, pi+1, high) 
 		
 print("Foram lidos: " + str(len(data)) + " Blood Messages")
-arr1 = data
+arr1 = messages
 begin = time.time()
 quickSort(arr1, 0, len(arr1)-1)
 end = time.time()
 quick_time = end - begin
 
-arr2 = data
+arr2 = messages
 begin = time.time()
 mergeSort(arr2)
 end = time.time()
 merge_time = end - begin
 
-print('O Merge sort demorou {} segundo e o Quick sort demorou {} segundo'.format(quick_time, merge_time))
+print('O Merge sort, algoritmo estavel, demorou {} segundos, tendo feito {} atribuicoes e o Quick sort, instavel, demorou {} segundos, tendo feito {} atribuicoes'.format(merge_time, mergeIteration, quick_time, quickIteration))
 
 Messages_per_world_X = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [],
 					  6: [], 7: [], 8: [], 9: [], 10: [], 11: [], 
@@ -125,20 +136,20 @@ Messages_per_world_Y = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [],
 					  12: [], 13: [], 14: [], 15: [], 16: [], 17: [], 18: []}
 					  
 for each in arr1:
-	Messages_per_world_X[each['WorldNumber']].append(each['XCoord'])
-	Messages_per_world_Y[each['WorldNumber']].append(each['YCoord'])
+	Messages_per_world_X[each.WorldNumber].append(each.XCoord)
+	Messages_per_world_Y[each.WorldNumber].append(each.YCoord)
 
 
+# Ignorando os mundos [1:9] pois nao possuem mensagens
+for i in range(9):
+	fig, ax = pyplot.subplots()
 
-#for i in range(9):
-#	fig, ax = pyplot.subplots()
-#
 	pyplot.xlabel('Position X')
 	pyplot.ylabel('Position Y')
-#	ax.scatter(Messages_per_world_X[i+10], Messages_per_world_Y[i+10], label='World: '+str(i+10))
+	ax.scatter(Messages_per_world_X[i+10], Messages_per_world_Y[i+10], label='World: '+str(i+10))
 	
-#	pyplot.legend()
+	pyplot.legend()
 
-#	pyplot.show()
+	pyplot.show()
 	
 	
